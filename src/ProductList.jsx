4f34e6e90caace6756f300c7b11c7daa696bed21
sasha55549 <sashakozwork@gmail.com>
2024-10-07 11:49:32 -0400

    message

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,11 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-
+    const [addedToCart, setAddedToCart] = useState({});
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -241,6 +242,9 @@ const handlePlantsClick = (e) => {
     setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
     setShowCart(false); // Hide the cart when navigating to About Us
 };
+   const handleAddToCart= (plant) => {
+
+   };
 
    const handleContinueShopping = (e) => {
     e.preventDefault();
@@ -268,7 +272,23 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
+            {plantsArray.map((category,index)=>(
+                <div key={index}>
+                    <h1><div>{category.category}</div></h1>
+                    <div className='product-list'>
+                        {category.plants.map((plant,pIndex)=>(
+                            <div className='product-card'>
+                                <img className='product-image' src={plant.image} alt={plant.name}/>
+                                <div className='product-title'>{plant.name}</div>
+                                <div className='product-description'>{plant.description}</div>
+                                <div className='product-price'>{plant.cost}</div>
+                                <button className='product-button' onClick={()=>handleAddToCart(plant)}>Add to Cart</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
 
+            ))}
 
         </div>
  ) :  (
